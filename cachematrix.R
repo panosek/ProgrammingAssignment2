e82ec8d83cf52a855d049a7ff04d4107ba92208c
panosek <panosek@peak.org>
2014-10-21 23:38:09 -0700

    cleaned up the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,37 @@
-## Matrix inversion is usually a costly computation. For large enough matrices it is beneficial to cache
-## the inverse of a matrix rather than computing it repeatedly . 
-## The pair of functions below cache the inverse of a matrix.
+## Matrix inversion is usually a costly computation. For large enough matrices it is
+## beneficial to cache the inverse of a matrix rather recomputing it. 
 
 
 
-## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
+## makeCacheMatrix: This function creates and returns a list of functions to:
+## set a matrix; get a matrix; set the inverse matrix; get the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 
         inv <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                inv <<- NULL
         }
-        get <- function() x
+        get <- function(){ return(x) }
+        
         setinverse <- function(inverse) {inv <<- inverse}
-        getinverse <- function() {inv}
-        list(set = set, get = get,
+        
+        getinverse <- function() {return(inv)}
+        
+        return(list(set = set, get = get,
              setinverse = setinverse,
-             getinverse = getinverse)
+             getinverse = getinverse))
 
 }
 
 
 
-##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
-## Return a matrix that is the inverse of 'x'
+##cacheSolve: This function computes and returns the inverse of the "matrix" previously passed to 
+## or set by makeCacheMatrix above.
+## If the inverse was previously calculated it retrieves the inverse from cache.
 
 cacheSolve <- function(x, ...) {
-
-
-##For example, if X is a square invertible matrix, then solve(X) returns its inverse
 
         inv <- x$getinverse()
         if(!is.null(inv)) {
@@ -40,6 +41,6 @@ cacheSolve <- function(x, ...) {
         mat <- x$get()
         inv <- solve(mat)
         x$setinverse(inv)
-        inv
+        return(inv)
        
 }
